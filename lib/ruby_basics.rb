e40fff0075028ba05a4ba6cb0567b84ade80df12
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-18 10:18:45 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -20,8 +20,8 @@ def return_a_value(lowercaseDummy)
   return lowercaseDummy
 end
 
-def last_evaluated_value(arg1)
-   "expert"
+def last_evaluated_value(arg1 = "expert")
+   arg1
 end
 
 def pizza_party(vegan="cheese")
